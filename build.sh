52397f39704b1b5a17aac0bbc1153c8408b1a2cd
Numbers00 <kmaquino@up.edu.ph>
2023-08-11 16:50:47 +0800

    Add base deployment pipeline action for exer 11.20

--- build.sh
+++ build.sh
@@ -1,24 +1,9 @@
 #!/bin/bash
-echo "node v"
-node -v
-echo "npm v"
-npm -v
-echo "ls-1"
-ls
+
 # set up frontend build
 cd frontend
-echo "ls-2"
-ls
 npm install
 npm run build:linux
-echo "ls-3"
-ls
 mv dist ../backend/dist
-echo "ls-4"
-ls
 cd ../backend
 npm install
-echo "ls-5"
-ls
-echo "ls-6"
-ls dist
--- start.sh
+++ start.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
 # run server in production mode with frontend build
-echo "ls-7"
-ls
 cd backend
-echo "ls-8"
-ls
 npm start
